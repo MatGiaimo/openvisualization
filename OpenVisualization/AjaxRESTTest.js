7bf56f9821dd3f204acf200cd8ec2bb4722be9b5
mgiaimo <mgiaimo@8477c67e-144c-11df-ae5d-f30dee00ba09>
2010-03-17 21:01:20 +0000

    Added an AJAX example using jquery.

---
+++ OpenVisualization/AjaxRESTTest.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function() {
+    var submitButton = $('#submit');
+
+    submitButton.click(function(e) {
+        // checked box
+        var xmlChoice = $('.xmlChoice:checked').val();
+        var xmlToLoad = "/Configuration/Charts/";
+        var xmlChoice;
+
+        xmlToLoad = xmlToLoad + xmlChoice;
+
+        $.ajax({
+            type: "GET",
+            url: xmlToLoad,
+            dataType: "xml",
+            success: function(xml) {
+                loadChartImage(xml);
+            }
+        });
+    });
+});
+
+function loadChartImage(xml) {
+    var resultsDiv = $('#chartResults');
+
+    $.ajax({
+        url: "/Services/GetStaticChartImage.aspx",
+        type: "POST",
+        processData: false,
+        contentType: "text/xml",
+        data: xml,
+        success: function(data) {
+            resultsDiv.html(data);
+        }
+    });
+}
